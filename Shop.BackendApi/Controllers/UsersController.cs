cf5afdab028746403e1b47b7d9e4e5e363844400
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-05-21 14:33:27 +0700

    add login api

---
+++ Shop.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.System.Users;
+using Shop.ViewModels.System.Users;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace Shop.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public readonly IUserService _userService;
+        public UsersController(IUserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid) 
+                return BadRequest(ModelState);
+
+            var resultToken = await _userService.Authenticate(request);
+
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or Password is incorrect");
+            }
+
+
+            return Ok(new { token = resultToken });
+        }
+
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.Register(request);
+
+            if (!result)
+            {
+                return BadRequest("Register is not successful");
+            }
+
+
+            return Ok();
+        }
+
+
+
+
+    }
+}
