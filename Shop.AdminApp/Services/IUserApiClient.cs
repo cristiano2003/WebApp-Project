c0a907d215cad9c4c5b7fa7b93ae8f686ac8d4ed
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-05-22 18:49:05 +0700

    cookie authentication

---
+++ Shop.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,10 @@
+﻿using Shop.ViewModels.System.Users;
+using System.Threading.Tasks;
+
+namespace Shop.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
